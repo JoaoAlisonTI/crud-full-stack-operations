e502211e8026b827b2ce33e4042a9c8733a9c5dd
JoaoAlisonTI <joaoalison.ti@gmail.com>
2023-08-05 15:49:40 -0300

    Add new deploy render

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 require("dotenv").config();
 
-const routes = require("../routes/TaskRoute")
+const routes = require("./routes/TaskRoute")
 
 const cors = require("cors");
 
